d9f0f30d975b5958c637998aba119d57b2b6d160
Erik Hedberg <erik@hedberg.at>
2018-07-07 15:21:29 +0200

    Allow arbitrary payload

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -4,15 +4,26 @@ import fetch from 'node-fetch';
 import requireAuthentication from './../middleware/authentication';
 const router = express.Router();
 
+const safeParse = stringifiedJson => {
+  try {
+    return JSON.parse(stringifiedJson);
+  } catch (e) {
+    return {};
+  }
+};
+
 router.all('*', requireAuthentication);
 router.get('/', (req, res) => {
-  const {domain, event, timestamp} = req.query;
+  const {domain, payload} = req.query;
+  const output = Object.entries(safeParse(payload))
+    .map(([key, value]) => `${key}: ${value}`)
+    .join('\n');
   fetch(process.env.SLACK_URL, {
     method: 'POST',
     body: JSON.stringify({
       username: 'Pintifier',
       icon_emoji: ':beer:',
-      text: `${domain}: ${event} @ ${timestamp}`,
+      text: `${domain}:\n${output}`,
     }),
   });
 
